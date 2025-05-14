1e4c0e386d44fa92ccdeb7506e5dd4c160d58a36
Joseph Eggers <eggersjoseph23@gmail.com>
2025-05-13 20:33:52 -0700

    temp: WIP saving multiple

--- bestTimeToBuyAndSellStock.cpp
+++ bestTimeToBuyAndSellStock.cpp
@@ -1,5 +1,6 @@
 // https://leetcode.com/problems/best-time-to-buy-and-sell-stock/
-
+// This is probably faster but less human readable;
+// Good way to think about optimization and what we are trying to maximize.
 class Solution {
 public:
     int maxProfit(vector<int>& prices) {
@@ -19,3 +20,23 @@ public:
         
     }
 };
+
+int maxProfit(vector<int>& prices) {
+        int minimum = prices[0];
+        int maxProfit = 0;
+        for (int i = 1; i < prices.size(); i++){
+            if (prices[i] > prices[i-1]) {
+                // increasing slope
+                int tempProfit = prices[i] - minimum;
+                if (tempProfit < maxProfit) continue;
+                maxProfit = tempProfit;
+            } else {
+                // decreasing slope
+                if( minimum < prices[i] ) continue;
+                minimum = prices[i]; 
+            }
+            
+        }
+        return maxProfit;
+        
+    }
